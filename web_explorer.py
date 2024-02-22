5ad924934b26f494fbf0bcaf4f726c5a6822eb82
Konstantin Krestnikov <KNKrestnikov@sberbank.ru>
2024-02-22 16:37:38 +0300

    Migrate to gigacode

--- web_explorer.py
+++ web_explorer.py
@@ -2,45 +2,68 @@ import streamlit as st
 from langchain.callbacks.base import BaseCallbackHandler
 from langchain.chains import RetrievalQAWithSourcesChain
 from langchain.retrievers.web_research import WebResearchRetriever
+from langchain_community.embeddings import GigaChatEmbeddings
+from langchain.text_splitter import RecursiveCharacterTextSplitter, TextSplitter
+
 
 import os
 
-os.environ["GOOGLE_API_KEY"] = "YOUR_API_KEY" # Get it at https://console.cloud.google.com/apis/api/customsearch.googleapis.com/credentials
-os.environ["GOOGLE_CSE_ID"] = "YOUR_CSE_ID" # Get it at https://programmablesearchengine.google.com/
-os.environ["OPENAI_API_BASE"] = "https://api.openai.com/v1"
-os.environ["OPENAI_API_KEY"] = "YOUR_API_KEY" # Get it at https://beta.openai.com/account/api-keys
 
 st.set_page_config(page_title="Interweb Explorer", page_icon="🌐")
+
 
 def settings():
 
     # Vectorstore
     import faiss
-    from langchain.vectorstores import FAISS 
+    from langchain.vectorstores import FAISS
     from langchain.embeddings.openai import OpenAIEmbeddings
-    from langchain.docstore import InMemoryDocstore  
-    embeddings_model = OpenAIEmbeddings()  
-    embedding_size = 1536  
-    index = faiss.IndexFlatL2(embedding_size)  
-    vectorstore_public = FAISS(embeddings_model.embed_query, index, InMemoryDocstore({}), {})
+    # from langchain_community.embeddings.gigachat import GigaChatEmbeddings
+    from langchain.docstore import InMemoryDocstore
+
+
+    # embeddings_model = OpenAIEmbeddings()
+    embeddings_model = GigaChatEmbeddings(
+        base_url="https://wmapi-ift.saluteai-pd.sberdevices.ru/v1",
+        model="Embeddings",
+        verify_ssl_certs=False,
+    )
+    embedding_size = 1536
+    # embedding_size = 128
+    index = faiss.IndexFlatL2(embedding_size)
+    vectorstore_public = FAISS(
+        embeddings_model.embed_query, index, InMemoryDocstore({}), {}
+    )
 
     # LLM
-    from langchain.chat_models import ChatOpenAI
-    llm = ChatOpenAI(model_name="gpt-3.5-turbo-16k", temperature=0, streaming=True)
+    # from langchain.chat_models import ChatOpenAI
+    from langchain_community.chat_models import GigaChat
+
+    # llm = ChatOpenAI(model_name="gpt-3.5-turbo-16k", temperature=0, streaming=True)
+    llm = GigaChat(
+        streaming=True,
+        verbose=True,
+        temperature=0,
+        model="GigaChat-29b-8k-funcs",
+        base_url="https://wmapi-dev.saluteai-pd.sberdevices.ru/v1/",
+        verify_ssl_certs=False,
+    )
 
     # Search
     from langchain.utilities import GoogleSearchAPIWrapper
-    search = GoogleSearchAPIWrapper()   
 
-    # Initialize 
+    search = GoogleSearchAPIWrapper()
+
+    # Initialize
     web_retriever = WebResearchRetriever.from_llm(
         vectorstore=vectorstore_public,
-        llm=llm, 
-        search=search, 
-        num_search_results=3
+        llm=llm,
+        search=search,
+        num_search_results=2,
     )
 
     return web_retriever, llm
+
 
 class StreamHandler(BaseCallbackHandler):
     def __init__(self, container, initial_text=""):
@@ -69,30 +92,35 @@ class PrintRetrievalHandler(BaseCallbackHandler):
 
 st.sidebar.image("img/ai.png")
 st.header("`Interweb Explorer`")
-st.info("`I am an AI that can answer questions by exploring, reading, and summarizing web pages."
-    "I can be configured to use different modes: public API or private (no data sharing).`")
+st.info(
+    "`Я - искусственный интеллект, который может отвечать на вопросы, исследуя, читая и кратко излагая содержание веб-страниц."
+    "Меня можно настроить на использование различных режимов: публичного API или приватного (без обмена данными).`"
+)
 
 # Make retriever and llm
-if 'retriever' not in st.session_state:
-    st.session_state['retriever'], st.session_state['llm'] = settings()
+if "retriever" not in st.session_state:
+    st.session_state["retriever"], st.session_state["llm"] = settings()
 web_retriever = st.session_state.retriever
 llm = st.session_state.llm
 
-# User input 
-question = st.text_input("`Ask a question:`")
+# User input
+question = st.text_input("`Ваш вопрос:`")
 
 if question:
 
     # Generate answer (w/ citations)
     import logging
+
     logging.basicConfig()
-    logging.getLogger("langchain.retrievers.web_research").setLevel(logging.INFO)    
+    logging.getLogger("langchain.retrievers.web_research").setLevel(logging.INFO)
     qa_chain = RetrievalQAWithSourcesChain.from_chain_type(llm, retriever=web_retriever)
 
     # Write answer and sources
     retrieval_streamer_cb = PrintRetrievalHandler(st.container())
     answer = st.empty()
     stream_handler = StreamHandler(answer, initial_text="`Answer:`\n\n")
-    result = qa_chain({"question": question},callbacks=[retrieval_streamer_cb, stream_handler])
-    answer.info('`Answer:`\n\n' + result['answer'])
-    st.info('`Sources:`\n\n' + result['sources'])
+    result = qa_chain(
+        {"question": question}, callbacks=[retrieval_streamer_cb, stream_handler]
+    )
+    answer.info("`Answer:`\n\n" + result["answer"])
+    st.info("`Sources:`\n\n" + result["sources"])
